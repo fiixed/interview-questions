4a07165b74a8d310f311e57d680943de41d4d628
fiixed <fiixed@gmail.com>
2020-09-17 09:07:26 -0400

    alt solution

--- reverse-string.js
+++ reverse-string.js
@@ -1,5 +1,14 @@
 /*jshint esversion: 6 */
 
-var reverseString = (str) => str.split("").reverse().join("");
+//var reverseString = (str) => str.split("").reverse().join("");
+
+function reverseString(str) {
+  let reversed = "";
+  for (const char of str) {
+    reversed = char + reversed;
+  }
+
+  return reversed;
+}
 
 console.log(reverseString("Hello"));

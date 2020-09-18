bf714abcd3c8300ae7b4e7184b47fb28ac469fee
fiixed <fiixed@gmail.com>
2020-09-18 17:01:49 -0400

    additional solution

--- palindromes.js
+++ palindromes.js
@@ -1,11 +1,17 @@
 /*jshint esversion: 6 */
 
+// function palindrome(str) {
+//   let reversed = "";
+//   for (const char of str) {
+//     reversed = char + reversed;
+//   }
+//   return reversed === str;
+// }
+
 function palindrome(str) {
-  let reversed = "";
-  for (const char of str) {
-    reversed = char + reversed;
-  }
-  return reversed == str ? true : false;
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 console.log(palindrome("abba"));

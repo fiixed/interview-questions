4db62507de62fd9ccc375efbe40ea052b16f39a4
fiixed <fiixed@gmail.com>
2020-09-21 09:49:43 -0400

    array-chunk

---
+++ array-chunk.js
@@ -0,0 +1,26 @@
+/*jshint esversion: 6 */
+// function chunk(array, size) {
+//   const chunked = [];
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1];
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//   return chunked;
+// }
+
+function chunk(array, size) {
+  const chunked = [];
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+  return chunked;
+}
+
+console.log(chunk([1, 2, 3, 4], 2));

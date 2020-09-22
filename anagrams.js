c411eb2a928b3fc902305b47c685f71734f13b7e
fiixed <fiixed@gmail.com>
2020-09-22 09:08:55 -0400

    alternative anagram

--- anagrams.js
+++ anagrams.js
@@ -1,27 +1,35 @@
 /*jshint esversion: 6 */
 
+// function anagrams(strA, strB) {
+//   const aCharMap = buildCharMap(strA);
+//   const bCharMap = buildCharMap(strB);
+
+//   if (Object.keys(aCharMap).length != Object.keys(bCharMap).length) {
+//     return false;
+//   }
+
+//   for (const char in aCharMap) {
+//     if (aCharMap[char] != bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function buildCharMap(str) {
+//   const charMap = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     charMap[char] = charMap[char] + 1 || 1;
+//   }
+//   return charMap;
+// }
+
 function anagrams(strA, strB) {
-  const aCharMap = buildCharMap(strA);
-  const bCharMap = buildCharMap(strB);
-
-  if (Object.keys(aCharMap).length != Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for (const char in aCharMap) {
-    if (aCharMap[char] != bCharMap[char]) {
-      return false;
-    }
-  }
-  return true;
+  return cleanString(strA) === cleanString(strB);
 }
 
-function buildCharMap(str) {
-  const charMap = {};
-  for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
-    charMap[char] = charMap[char] + 1 || 1;
-  }
-  return charMap;
+function cleanString(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 }
 
 console.log(anagrams("hello", "olleh"));

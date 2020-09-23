e0e1fb92bb6b6bd8d2d179bb27c413978bfca618
fiixed <fiixed@gmail.com>
2020-09-23 09:56:19 -0400

    alternative solution

--- capitalize.js
+++ capitalize.js
@@ -7,12 +7,16 @@ function capitalize(str) {
   return words.join(" ");
 }
 
-function capitalize(str) {
-    const words = [];
-    for (const word of str.split(" ")) {
-      words.push(word[0].toUpperCase() + word.slice(1));
-    }
-    return words.join(" ");
-  }
+// function capitalize(str) {
+//     let result = str[0].toUpperCase();
+//     for (let i = 1; i < str.length; i++){
+//         if (str[i - 1] === ' ') {
+//             result += str[i].toUpperCase();
+//         } else {
+//             result += str[i];
+//         }
+//     }
+//     return result;
+//   }
 
 console.log(capitalize("a short sentence"));

bd686cc062312c78fdee598ac2217b15ce193800
fiixed <fiixed@gmail.com>
2020-09-29 09:46:17 -0500

    added directions

--- reverse-string.js
+++ reverse-string.js
@@ -1,5 +1,13 @@
 /*jshint esversion: 6 */
 
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
 //var reverseString = (str) => str.split("").reverse().join("");
 
 function reverseString(str) {
--- steps.js
+++ steps.js
@@ -1,5 +1,24 @@
 /*jshint esversion: 6 */
 
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+// --- Examples
+//   steps(2)
+//       '# '
+//       '##'
+//   steps(3)
+//       '#  '
+//       '## '
+//       '###'
+//   steps(4)
+//       '#   '
+//       '##  '
+//       '### '
+//       '####'
+
 function steps(num) {
   for (let row = 0; row < num; row++) {
     let stair = "";

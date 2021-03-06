d06d841df04c52f12b6a26b509019e9bee2d08b3
fiixed <fiixed@gmail.com>
2021-03-06 09:31:40 -0500

    reverse practice

--- reverse-string.js
+++ reverse-string.js
@@ -11,12 +11,13 @@
 //var reverseString = (str) => str.split("").reverse().join("");
 
 function reverseString(str) {
-  let reversed = "";
+  let reversed = '';
   for (const char of str) {
     reversed = char + reversed;
   }
-
   return reversed;
 }
 
 console.log(reverseString("Hello"));
+
+
---
+++ scratch.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var removeDuplicates = function(nums) {
+     let newArr = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (i == i + 1) {
+            
+        }
+        
+    }
+};
